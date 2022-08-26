0476ebc811fe446fcc91098e595dbb962ddd7103
Victor Sanavia <vsanavia@gmail.com>
2022-08-26 17:02:05 -0500

    Resize icon image and entrypoint changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,9 @@
 #!/bin/bash
 
-#TIME_TO_SHUTDOWN=3600
-
-#mkdir -p /home/pyuser/notebook
-
-# In case fly volumes put something there
-#rm -rf '/home/pyuser/notebook/lost+found'
-
-#if [ -z "$(ls -A /home/pyuser/notebook)" ]; then
-    #echo "Preparing notebook"
-    #rm -rf /home/pyuser/notebook
-    #git clone $GIT_REPO /notebook
-
-    #cd /home/pyuser/notebook
-
-#fi
-
-#code-server --bind-addr 0.0.0.0:9090 /home/pyuser/notebook #&
+code-server /home/pyuser/notebook &
     #/tired-proxy --port 8080 --host http://localhost:9090 --time $TIME_TO_SHUTDOWN
 #su pyuser
-/home/pyuser/.venv/bin/jupyter notebook --ip=0.0.0.0 --port=9999
+/home/pyuser/.venv/bin/jupyter notebook --ip=0.0.0.0 --port=8787 --certfile=/home/pyuser/.tls/localhost.pem \
+--keyfile=/home/pyuser/.tls/localhost.key --no-browser --allow-root --NotebookApp.token='' \
+--NotebookApp.password='' --NotebookApp.allow_origin='*' --NotebookApp.base_url=/notebook \
+--NotebookApp.notebook_dir=/home/pyuser/notebook
