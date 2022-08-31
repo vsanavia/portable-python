338c248c4c6fdf63da88b62f2d6b3780081878ed
Victor Sanavia <vsanavia@gmail.com>
2022-08-30 19:12:29 -0500

    Run jupyter lan on entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 code-server --bind-addr 0.0.0.0:9090 /home/pyuser/notebook &
     #/tired-proxy --port 8080 --host http://localhost:9090 --time $TIME_TO_SHUTDOWN
 #su pyuser
-/home/pyuser/.venv/bin/jupyter notebook --ip=0.0.0.0 --port=8787 --certfile=/home/pyuser/.tls/localhost.pem \
+/home/pyuser/.venv/bin/jupyter lab --ip=0.0.0.0 --port=8787 --certfile=/home/pyuser/.tls/localhost.pem \
 --keyfile=/home/pyuser/.tls/localhost.key --no-browser --allow-root --NotebookApp.token='' \
 --NotebookApp.password='' --NotebookApp.allow_origin='*' --NotebookApp.base_url=/notebook \
 --NotebookApp.notebook_dir=/home/pyuser/notebook
